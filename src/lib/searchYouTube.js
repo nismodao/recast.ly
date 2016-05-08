cd92e55bb48ef0b8ece1814a33fe529caa831056
nismodao <nam.d.dao@gmail.com>
2016-05-07 19:21:04 -0700

    Completed Search Youtube portion

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
 var searchYouTube = (options, callback) => {
+
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      part: 'snippet',
+      type: 'video',
+      key: options.key,
+      q: options.query,
+      maxResults: options.max
+    },
+    success: function (response) {
+      console.log("response is",response.items);
+      callback(response.items);
+    }
+  });
   // TODO
 };
 
 window.searchYouTube = searchYouTube;
+
+
+
+// $('.form-control').text() + '&format=5'
